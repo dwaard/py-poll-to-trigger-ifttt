b38291db4cc241bd03d440fd95f43d73339f6e3e
Daan de Waard <ponwdd@pontes.nl>
2021-03-19 13:05:16 +0100

    Add http request

--- hello.py
+++ hello.py
@@ -1,3 +1,8 @@
-# https://maker.ifttt.com/trigger/dwaard_sent_request/with/key/UTRIYrT2PX4IKIdhrHigb
+# importing the requests library 
+import requests 
 
-print("hello")
+# api-endpoint 
+URL = "https://maker.ifttt.com/trigger/dwaard_sent_request/with/key/UTRIYrT2PX4IKIdhrHigb"
+
+# sending get request and saving the response as response object 
+r = requests.get(url = URL) 
